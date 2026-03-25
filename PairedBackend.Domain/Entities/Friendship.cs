dff0cefe32aa7e5991c7aed25d8a858f7bdafaf1
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-03-25 20:40:07 +0100

    feat: add friendship domain entity

---
+++ PairedBackend.Domain/Entities/Friendship.cs
@@ -0,0 +1,21 @@
+﻿namespace PairedBackend.Domain.Entities;
+
+public class Friendship
+{
+    public Guid UserId { get; private set; }
+    public Guid FriendId { get; private set; }
+
+    public DateTime CreatedAt { get; private set; }
+
+    private Friendship() { }
+
+    public Friendship(Guid userId, Guid friendId)
+    {
+        if (userId == friendId)
+            throw new ArgumentException("Cannot be friend with yourself");
+
+        UserId = userId;
+        FriendId = friendId;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
