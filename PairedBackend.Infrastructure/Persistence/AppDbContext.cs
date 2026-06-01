04f6ffcc3b78561ad798265f87b5216ff57d252a
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-06-01 17:27:52 +0200

    feat: add initial create migration

--- PairedBackend.Infrastructure/Persistence/AppDbContext.cs
+++ PairedBackend.Infrastructure/Persistence/AppDbContext.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 
 namespace PairedBackend.Infrastructure.Persistence;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
 {
     public DbSet<Friendship> Friendships => Set<Friendship>();
 
