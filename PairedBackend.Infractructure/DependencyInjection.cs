dc332b2ef834efdb7e8d16305dfac45f33c7c254
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-06-01 16:50:19 +0200

    feat: add infrustructure dependency injection

---
+++ PairedBackend.Infractructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PairedBackend.Infractructure.Identity;
+using PairedBackend.Infractructure.Persistence;
+
+namespace PairedBackend.Infractructure;
+
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"));
+        });
+        services
+            .AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+        
+        return services;
+    }
+}
