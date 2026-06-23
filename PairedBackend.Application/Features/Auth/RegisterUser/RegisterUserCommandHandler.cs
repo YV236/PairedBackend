654c43c925a0ed355173ada2afb793eecd76c525
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-06-23 14:23:19 +0200

    feat: add register user command and handler

---
+++ PairedBackend.Application/Features/Auth/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using PairedBackend.Domain.Shared;
+
+namespace PairedBackend.Application.Features.Auth.RegisterUser;
+
+internal class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Result<Guid>>
+{
+    public Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
