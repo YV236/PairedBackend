99149488a811d4688e7ed4cd6950e0350cbcb8f8
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-06-23 14:27:31 +0200

    feat: add user register contracts

---
+++ PairedBackend.Contracts/User/RegisterUserRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace PairedBackend.Contracts.User;
+
+public class RegisterUserRequest
+{
+    public required string Email { get; set; }
+
+    public required string UserName { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+}
---
+++ PairedBackend/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PairedBackend.Contracts.User;
+
+namespace PairedBackend.API.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [Route("register")]
+    public IActionResult Register(RegisterUserRequest request)
+    {
+        return Ok();
+    }
+}
