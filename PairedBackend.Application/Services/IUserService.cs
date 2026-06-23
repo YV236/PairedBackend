c7f1befcb96a7840fa3a7c386c5bcc5a0e2e9456
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-06-23 14:25:56 +0200

    feat: add infrustructure user service

---
+++ PairedBackend.Application/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using PairedBackend.Domain.Shared;
+
+namespace PairedBackend.Application.Services;
+
+public interface IUserService
+{
+    Task<Result<Guid>> RegisterAsync(
+        string email,
+        string username,
+        string password,
+        string firstName,
+        string lastName,
+        CancellationToken cancellationToken);
+}
---
+++ PairedBackend.Infrastructure/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using PairedBackend.Application.Services;
+using PairedBackend.Domain.Shared;
+
+namespace PairedBackend.Infrastructure.Services;
+
+internal class UserService : IUserService
+{
+    public Task<Result<Guid>> RegisterAsync(string email, string username, string password, string firstName, string lastName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
