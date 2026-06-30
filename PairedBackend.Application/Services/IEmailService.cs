e5213e53ffa8d4bd927550c49f5f5c9e4cced26e
PH0EN1X\yurav <yuravinnyk67@gmail.com>
2026-06-30 14:10:13 +0200

    feat: add user and email services

---
+++ PairedBackend.Application/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using PairedBackend.Domain.Shared;
+
+namespace PairedBackend.Application.Services;
+
+public interface IEmailService
+{
+    public Task<Result> SendConfirmationEmail(string userEmail);
+
+    public Task<Result> SendResetEmail(string userEmail);
+}
---
+++ PairedBackend.Application/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using PairedBackend.Domain.Shared;
+
+namespace PairedBackend.Application.Services;
+
+public interface IUserService
+{
+    public Task<Result> ChangeUserInfo(string firstName, string lastName, string userName);
+
+    public Task<Result> ChangeProfilePicture();
+
+    public Task<Result> AddMusicPlatform();
+
+    public Task<Result> RemoveMusicPlatform();
+}
